00db49383c51302f17b444639fa46ec5ee0c233e
Danerose <valencia3698@gmail.com>
2023-06-08 01:44:22 -0500

    get id from url

--- lib/core/constants/pokemon_api.constants.dart
+++ lib/core/constants/pokemon_api.constants.dart
@@ -1,4 +1,7 @@
 class PokemonApiConstants {
   static const String pokemon = '/pokemon';
-  static String pokemonDetail(String url) => '/pokemon';
+  static String pokemonDetail(String url) {
+    final size = url.split('/');
+    return '/pokemon/${size[size.length - (url.endsWith('/') ? 2 : 1)]}';
+  }
 }
