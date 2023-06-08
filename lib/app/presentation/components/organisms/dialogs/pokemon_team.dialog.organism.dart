04d0e65a42d5c36f4bad9350381c948867c66e89
Danerose <valencia3698@gmail.com>
2023-06-08 17:50:25 -0500

    Add Yuor team is empty ui

--- lib/app/presentation/components/organisms/dialogs/pokemon_team.dialog.organism.dart
+++ lib/app/presentation/components/organisms/dialogs/pokemon_team.dialog.organism.dart
@@ -24,26 +24,33 @@ class PokemonTeamDialogOrganism extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           const SizedBox(height: 30),
-          Column(
-            children: List.generate(team.length, (index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  dense: true,
-                  leading: ImageMolecule(
-                    url: team[index].detail.sprite.front,
-                    width: injector.get<SizeConfig>().sizeH * 10,
-                    height: injector.get<SizeConfig>().sizeH * 10,
-                    borderRadius: BorderRadius.circular(10),
+          Visibility(
+            visible: team.isNotEmpty,
+            replacement: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(context.l10n.yourTeamIsEmpty),
+            ),
+            child: Column(
+              children: List.generate(team.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    dense: true,
+                    leading: ImageMolecule(
+                      url: team[index].detail.sprite.front,
+                      width: injector.get<SizeConfig>().sizeH * 10,
+                      height: injector.get<SizeConfig>().sizeH * 10,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () => onDelete(team[index]),
+                      icon: const Icon(Icons.delete_forever),
+                    ),
+                    title: Text(team[index].name),
                   ),
-                  trailing: IconButton(
-                    onPressed: () => onDelete(team[index]),
-                    icon: const Icon(Icons.delete_forever),
-                  ),
-                  title: Text(team[index].name),
-                ),
-              );
-            }),
+                );
+              }),
+            ),
           ),
           TextButton(
             onPressed: () {
