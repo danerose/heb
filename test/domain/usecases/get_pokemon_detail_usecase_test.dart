d499799c2781b2e84979b090b46213007d88cafe
Danerose <valencia3698@gmail.com>
2023-06-08 21:57:43 -0500

    get_pokemon_detail_usecases.test.usecase done

--- test/domain/usecases/get_pokemon_detail_usecase_test.dart
+++ test/domain/usecases/get_pokemon_detail_usecase_test.dart
@@ -1,1 +1,40 @@
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
 
+import 'package:flutter_test/flutter_test.dart';
+import '../../test_hepers/test_hepers.mocks.dart';
+
+import 'package:heb/app/domain/entities/sprites/sprite.entity.dart';
+import 'package:heb/app/domain/entities/pokemon_detail_response.entity.dart';
+
+import 'package:heb/app/domain/usecases/get_pokemon_detail.usecase.dart';
+
+void main() {
+  late MockPokemonRepository mockPokemonRepository;
+  late GetPokemonDetailUsecase usecaseDetail;
+
+  setUp(() {
+    mockPokemonRepository = MockPokemonRepository();
+    usecaseDetail = GetPokemonDetailUsecase(
+      pokemonRepository: mockPokemonRepository,
+    );
+  });
+
+  const pokemonDeail = PokemonDetailResponse(
+    sprite: Sprite.empty(),
+    types: [],
+  );
+
+  test(
+    'should get a PokemonDetailResponse from the repository',
+    () async {
+      // arrange
+      when(mockPokemonRepository.getPokemonDetail(url: ''))
+          .thenAnswer((_) async => const Right(pokemonDeail));
+      // act
+      final result = await usecaseDetail.execute(url: '');
+      // assert
+      expect(result, equals(const Right(pokemonDeail)));
+    },
+  );
+}
